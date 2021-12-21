23a2dff58540b824790e2b6c3989c3c0ce2ea460
sato_tats <satotats.alive@gmail.com>
2021-12-21 22:55:09 +0900

    define mock provider instead of using basic authentication

--- app/src/test/kotlin/com/satotats/ktor/AppTest.kt
+++ app/src/test/kotlin/com/satotats/ktor/AppTest.kt
@@ -3,21 +3,32 @@ package com.satotats.ktor
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 
 class AppTest {
     @Test
-    fun `authenticated hello`() {
+    fun `authenticated hello - success`() {
         withTestApplication({ testModule(loginUser = TestUser.Bezos) }) {
-            handleRequest(HttpMethod.Get, "/hello") {
-                addHeader(HttpHeaders.Authorization, anyAuthKey())
-            }.apply {
-                assertEquals(HttpStatusCode.OK, response.status())
-                assertEquals("Hello, ${TestUser.Bezos.id.value}", response.content)
+            handleRequest(HttpMethod.Get, "/hello") { }
+                .apply {
+                    assertEquals(HttpStatusCode.OK, response.status())
+                    assertEquals("Hello, ${TestUser.Bezos.id.value}", response.content)
 
-                // "Hello, amzn001"
-                println(response.content)
-            }
+                    // "Hello, amzn001"
+                    println(response.content)
+                }
+        }
+    }
+
+    @Test
+    fun `authenticated hello - failure`() {
+        withTestApplication({ testModule(/*loginUser = TestUser.Bezos*/) }) {
+            handleRequest(HttpMethod.Get, "/hello") { }
+                .apply {
+                    assertEquals(HttpStatusCode.Unauthorized, response.status())
+                    assertNull(response.content)
+                }
         }
     }
 }
--- app/src/test/kotlin/com/satotats/ktor/TestAuth.kt
+++ app/src/test/kotlin/com/satotats/ktor/TestAuth.kt
@@ -2,15 +2,54 @@ package com.satotats.ktor
 
 import io.ktor.application.*
 import io.ktor.auth.*
+import io.ktor.response.*
 
 fun Application.testAuth(loginUser: LoginUser? = null) {
     install(Authentication) {
-        basic {
-            authentication { }
-            validate { return@validate loginUser?.id }
+        mock {
+            principal { loginUser?.id }
         }
     }
 }
 
-private const val mockAuthKey =  "Basic amV0YnJhaW5zOmZvb2Jhcg"
-fun anyAuthKey() = mockAuthKey
+
+fun Authentication.Configuration.mock(
+    name: String? = null,
+    configure: MockAuthenticationProvider.Configuration.() -> Unit
+) {
+    val provider = MockAuthenticationProvider.Configuration(name).apply(configure).build()
+
+    provider.pipeline.intercept(AuthenticationPipeline.RequestAuthentication) { context ->
+        val principal = provider.principalProvider(call)
+
+        if (principal == null) {
+            // completing context.challenge is necessary,
+            // without that authentication will not fail
+            // even when the principal is null
+            call.respond(UnauthorizedResponse())
+            context.challenge.complete()
+            return@intercept
+        }
+
+        context.principal(principal)
+    }
+    register(provider)
+}
+
+class MockAuthenticationProvider(config: Configuration) : AuthenticationProvider(config) {
+    val principalProvider = config.principalProvider
+
+    class Configuration(name: String?) : AuthenticationProvider.Configuration(name) {
+        var principalProvider: ApplicationCall.() -> Principal? = { null }
+
+        fun build() = MockAuthenticationProvider(this)
+
+        /**
+         * if principalProvider returns null,
+         * authentication will fail(401 response will be returned)
+         * */
+        fun principal(principalProvider: ApplicationCall.() -> Principal?) {
+            this.principalProvider = principalProvider
+        }
+    }
+}
